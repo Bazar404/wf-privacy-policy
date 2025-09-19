294966ba5e470abd916ad7bb6ecce1709cd51a71
Alberto Ledesma <129901336+ledesmx@users.noreply.github.com>
2025-09-19 13:50:35 -0600

    Add gihub action to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://bazar404.github.io',
+  base: '/wf-privacy-policy',
 });
